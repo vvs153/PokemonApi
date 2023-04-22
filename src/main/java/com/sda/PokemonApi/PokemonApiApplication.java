6816b6a17fe7d66b856539566a12ae96420f18b3
Kuba Postol <vvs153@gmail.com>
2023-04-22 10:14:19 +0200

    initialize PokemonApi

---
+++ src/main/java/com/sda/PokemonApi/PokemonApiApplication.java
@@ -0,0 +1,14 @@
+package com.sda.PokemonApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonApiApplication.class, args);
+	}
+
+}
+git
