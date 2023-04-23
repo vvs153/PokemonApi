4f30f872d4c09d342cd93ef1c8aa1c65e204ec8e
Kuba Postol <vvs153@gmail.com>
2023-04-23 15:49:40 +0200

    test(paa): add missing FetchPokemonListUseCase unit tests

--- src/test/java/com/sda/PokemonApi/FetchPokemonListUseCaseTest.java
+++ src/test/java/com/sda/PokemonApi/FetchPokemonListUseCaseTest.java
@@ -3,6 +3,7 @@ package com.sda.PokemonApi;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,7 +22,7 @@ class FetchPokemonListUseCaseTest {
         PokemonList pokemonList = Mockito.mock(PokemonList.class);
         PokemonItemEntity pokemonItemEntity = Mockito.mock(PokemonItemEntity.class);
         Mockito.when(pokemonList.getNext()).thenReturn(null);
-        Mockito.when(pokemonList.getResults()).thenReturn(null);
+        Mockito.when(pokemonList.getResults()).thenReturn(Collections.singletonList(pokemonListItem));
         Mockito.when(networkRepository.fetchPokemonListResult(100,0)).thenReturn(pokemonList);
         Mockito.when(mapper.toEntity(pokemonListItem)).thenReturn(pokemonItemEntity);
         Mockito.when(itemRepository.saveAll(Collections.singletonList(pokemonItemEntity))).thenReturn(Collections.singletonList(pokemonItemEntity));
@@ -47,6 +48,48 @@ class FetchPokemonListUseCaseTest {
         //then
         assertTrue(result.isEmpty());
 
+    }
+    @Test
+    void given_network_api_when_fetching_pokemon_list_including_pagination_then_pokemon_list_should_be_mapped_to_entities_and_saved_in_repo() {
+        //given
+        PokemonApiNetworkRepository pokeApiListNetworkRepository = Mockito.mock(PokemonApiNetworkRepository.class);
+        PokemonApiItemRepository pokemonListItemRepository = Mockito.mock(PokemonApiItemRepository.class);
+        PokemonListItemMapper pokemonListItemMapper = Mockito.mock(PokemonListItemMapper.class);
+        FetchPokemonListUseCase fetchPokemonListUseCase = new FetchPokemonListUseCase(
+                pokemonListItemRepository,
+                pokeApiListNetworkRepository,
+                pokemonListItemMapper
+        );
+        PokemonListItem pokeApiListItemResult1 = Mockito.mock(PokemonListItem.class);
+        PokemonList pokeApiListResult1 = Mockito.mock(PokemonList.class);
+
+        PokemonListItem pokeApiListItemResult2 = Mockito.mock(PokemonListItem.class);
+        PokemonList pokeApiListResult2 = Mockito.mock(PokemonList.class);
+
+        Mockito.when(pokeApiListResult1.getNext()).thenReturn("test");
+        Mockito.when(pokeApiListResult1.getResults()).thenReturn(Collections.singletonList(pokeApiListItemResult1));
+
+        Mockito.when(pokeApiListResult2.getNext()).thenReturn(null);
+        Mockito.when(pokeApiListResult2.getResults()).thenReturn(Collections.singletonList(pokeApiListItemResult2));
+
+
+        PokemonItemEntity pokemonListItemEntity1 = Mockito.mock(PokemonItemEntity.class);
+        PokemonItemEntity pokemonListItemEntity2 = Mockito.mock(PokemonItemEntity.class);
+        List<PokemonItemEntity> expectedResults = Arrays.asList(pokemonListItemEntity1, pokemonListItemEntity2);
+
+        Mockito.when(pokeApiListNetworkRepository.fetchPokemonListResult(100, 0)).thenReturn(pokeApiListResult1);
+        Mockito.when(pokeApiListNetworkRepository.fetchPokemonListResult(100, 100)).thenReturn(pokeApiListResult2);
+
+        Mockito.when(pokemonListItemMapper.toEntity(pokeApiListItemResult1)).thenReturn(pokemonListItemEntity1);
+        Mockito.when(pokemonListItemMapper.toEntity(pokeApiListItemResult2)).thenReturn(pokemonListItemEntity2);
+
+        Mockito.when(pokemonListItemRepository.saveAll(expectedResults)).thenReturn(expectedResults);
+
+        //when
+        List<PokemonItemEntity> result = fetchPokemonListUseCase.execute();
+
+        //then
+        assertEquals(expectedResults, result);
     }
 
 }
