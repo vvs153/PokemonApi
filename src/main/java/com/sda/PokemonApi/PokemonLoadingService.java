99a8b28f42b813b2c96770c327c809a330662188
Kuba Postol <vvs153@gmail.com>
2023-05-20 09:23:27 +0200

    feature(paa): add updating data every hour

--- src/main/java/com/sda/PokemonApi/PokemonLoadingService.java
+++ src/main/java/com/sda/PokemonApi/PokemonLoadingService.java
@@ -1,36 +1,41 @@
 package com.sda.PokemonApi;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-
 @Service
+@EnableScheduling
 public class PokemonLoadingService {
 private final FetchPokemonListUseCase fetchPokemonListUseCase;
 private final PokemonApiItemRepository pokemonApiItemRepository;
 private final PokemonApiNetworkRepository pokemonApiNetworkRepository;
 
-    public PokemonLoadingService(FetchPokemonListUseCase fetchPokemonListUseCase, PokemonApiItemRepository pokemonApiItemRepository, PokemonApiNetworkRepository pokemonApiNetworkRepository) {
+    public PokemonLoadingService(FetchPokemonListUseCase fetchPokemonListUseCase,
+                                 PokemonApiItemRepository pokemonApiItemRepository,
+                                 PokemonApiNetworkRepository pokemonApiNetworkRepository) {
         this.fetchPokemonListUseCase = fetchPokemonListUseCase;
         this.pokemonApiItemRepository = pokemonApiItemRepository;
         this.pokemonApiNetworkRepository = pokemonApiNetworkRepository;
     }
 
-    @PostConstruct
-    void loadPokemonList(){
-        long repoCount = pokemonApiItemRepository.count();
-        if(repoCount==0){
-            fetchPokemonListUseCase.execute();
-        }
-        else {
-            PokemonList countResult = pokemonApiNetworkRepository.fetchPokemonListResult(1,0);
-            int apiResponseCount = countResult.getCount();
-            if(repoCount!=apiResponseCount){
-                pokemonApiItemRepository.deleteAll();
+
+    @Scheduled(fixedRate = 3600000,initialDelay = 100)
+    void loadPokemonList() {
+
+            long repoCount = pokemonApiItemRepository.count();
+            if (repoCount == 0) {
                 fetchPokemonListUseCase.execute();
+            } else {
+                PokemonList countResult = pokemonApiNetworkRepository.fetchPokemonListResult(1, 0);
+                int apiResponseCount = countResult.getCount();
+                if (repoCount != apiResponseCount) {
+                    pokemonApiItemRepository.deleteAll();
+                    fetchPokemonListUseCase.execute();
+                }
             }
-        }
+
     }
+
+
 }
