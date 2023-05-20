82f214a780a7e538f4b4516f6bf15744820df947
Kuba Postol <vvs153@gmail.com>
2023-05-20 15:33:27 +0200

    test(paa): fix

--- src/test/java/com/sda/PokemonApi/pokemon_details/PokemonDetailsServiceTest.java
+++ src/test/java/com/sda/PokemonApi/pokemon_details/PokemonDetailsServiceTest.java
@@ -36,27 +36,6 @@ class PokemonDetailsServiceTest {
         Mockito.verify(pokemonDetailsRepository).save(pokemonDetailsEntity);
     }
     @Test
-    void given_pokemon_details_stored_in_db_when_get_details_then_details_should_be_provided_from_db(){
-        PokemonApiItemRepository pokemonApiItemRepository = Mockito.mock(PokemonApiItemRepository.class);
-        PokemonApiDetailsNetworkRepository pokemonApiDetailsNetworkRepository = Mockito.mock(PokemonApiDetailsNetworkRepository.class);
-        PokemonDetailsMapper pokemonDetailsMapper = Mockito.mock(PokemonDetailsMapper.class);
-        PokemonDetailsRepository pokemonDetailsRepository = Mockito.mock(PokemonDetailsRepository.class);
-        PokemonDetailsService pokemonDetailsService = new PokemonDetailsService(pokemonApiItemRepository,pokemonApiDetailsNetworkRepository,pokemonDetailsMapper,pokemonDetailsRepository);
-        String pokemonName = "Bulbasaur";
-        PokemonDetailsEntity pokemonDetailsEntity = Mockito.mock(PokemonDetailsEntity.class);
-        PokemonItemEntity pokemonItemEntity = new PokemonItemEntity(1L, pokemonName);
-        Mockito.when(pokemonApiItemRepository.findByNameIgnoreCase(pokemonName)).thenReturn(Optional.of(pokemonItemEntity));
-        Mockito.when(pokemonDetailsMapper.toEntity(Mockito.any())).thenReturn(pokemonDetailsEntity);
-
-        //when
-        pokemonDetailsService.getPokemonDetailsUrl(pokemonName);
-
-        //then
-        Mockito.verify(pokemonApiDetailsNetworkRepository).fetchPokemonDetailsResult(1L);
-        Mockito.verify(pokemonDetailsRepository).save(pokemonDetailsEntity);
-
-    }
-    @Test
     void given_pokemon_not_exist_in_db_when_get_details_then_details_not_be_provided() {
         //given
         PokemonApiItemRepository pokemonApiItemRepository = Mockito.mock(PokemonApiItemRepository.class);
@@ -79,5 +58,27 @@ class PokemonDetailsServiceTest {
         Mockito.verify(pokemonApiDetailsNetworkRepository, Mockito.never()).fetchPokemonDetailsResult(1L);
         Mockito.verify(pokemonDetailsRepository, Mockito.never()).save(Mockito.any());
     }
+    @Test
+    void given_pokemon_details_stored_in_db_when_get_details_then_details_should_be_provided_from_db(){
+        PokemonApiItemRepository pokemonApiItemRepository = Mockito.mock(PokemonApiItemRepository.class);
+        PokemonApiDetailsNetworkRepository pokemonApiDetailsNetworkRepository = Mockito.mock(PokemonApiDetailsNetworkRepository.class);
+        PokemonDetailsMapper pokemonDetailsMapper = Mockito.mock(PokemonDetailsMapper.class);
+        PokemonDetailsRepository pokemonDetailsRepository = Mockito.mock(PokemonDetailsRepository.class);
+        PokemonDetailsService pokemonDetailsService = new PokemonDetailsService(pokemonApiItemRepository,pokemonApiDetailsNetworkRepository,pokemonDetailsMapper,pokemonDetailsRepository);
+        String pokemonName = "Bulbasaur";
+        PokemonDetailsEntity pokemonDetailsEntity = Mockito.mock(PokemonDetailsEntity.class);
+        PokemonItemEntity pokemonItemEntity = new PokemonItemEntity(1L, pokemonName);
+        Mockito.when(pokemonApiItemRepository.findByNameIgnoreCase(pokemonName)).thenReturn(Optional.of(pokemonItemEntity));
+        Mockito.when(pokemonDetailsMapper.toEntity(Mockito.any())).thenReturn(pokemonDetailsEntity);
+
+        //when
+        pokemonDetailsService.getPokemonDetailsUrl(pokemonName);
+
+        //then
+        Mockito.verify(pokemonApiDetailsNetworkRepository).fetchPokemonDetailsResult(1L);
+        Mockito.verify(pokemonDetailsRepository).save(pokemonDetailsEntity);
+
+    }
+
 
 }
