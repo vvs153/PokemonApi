4d9541e66a39a69ea20c23411ba4b3b47af807dc
Kuba Postol <vvs153@gmail.com>
2023-05-21 13:56:45 +0200

    feature(paa): add users and password encoder

--- src/main/java/com/sda/PokemonApi/config/SecurityConfig.java
+++ src/main/java/com/sda/PokemonApi/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.sda.PokemonApi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +18,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/pokemon/list").permitAll()
                 .and()
                 .httpBasic();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("test1").password(passwordEncoder().encode("test1")).roles("user")
+                .and()
+                .withUser("test2").password(passwordEncoder().encode("test2")).roles("user");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
